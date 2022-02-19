336b1c6c3fd317aef607a74918e196667e574945
greencodedev <greencodedev912@gmail.com>
2022-02-19 09:05:15 +0800

    complete

--- types/express/index.d.ts
+++ types/express/index.d.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
 
 declare global {
-  namespace Express {
-    interface Request {
-      responseData: any;
-      user: any;
+    namespace Express {
+        interface Request {
+            responseData: any;
+            user: any;
+        }
     }
-  }
 }
